fe6796f0c9ecc56742a00f7609f54895eb0adb4c
budiraharjo <boeday604@gmail.com>
2018-01-24 22:19:23 +0700

    versi 0.1

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" " ">
+<html xmlns=" ">
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Bank Darah</title>
+  <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.gif" />
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css"  media="screen" />
+  <link rel="stylesheet" type="text/css" href="assets/css/common.css" media="screen" />
+  <script src="assets/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+  <script src="assets/js/slides.min.jquery.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    $(function(){
+      $('.slides').slides({
+        preload: true,
+        generatePagination: true,
+        play:3000
+      });
+    });
+  </script>
+
+  </head>
+  <body>
+<!--Wrapper-->
+<div id="wrapper"> 
+      <!--Page-->
+      <div class="page shadow"> 
+    <!--Header-->
+    <div id="header">
+        <div class="primary-section">
+        <div class="logo"> <a href=" "><img src="assets/images/logo.png" alt="logo" style="width: 50px" title="webgranth" /></a></div>
+        <div class="header-aside"> 
+            <ul>
+            <li class="border"><a href="#" class="log"> login </a></li>
+            <li><a href="#" class="user">register</a></li>
+            </ul>
+            </div>
+		</div>
+          <div class="nav-section">
+        <ul class="navigation">
+              <li class="home"><a href="index.html" class="home">Home </a></li>
+              <li><a href="#">Service </a></li>
+              <li><a href="#">Shops </a></li>
+              <li><a href="#">Reviews</a></li>
+              <li><a href="#">New Products </a></li>
+              <li><a href="#">Specials </a></li>
+              <li><a href="#">Contact </a></li>
+            </ul>
+        <ul class="search-box">
+              <li>
+            <input type="text" value="search" id="user_search" autocomplete="off" onkeypress="submitenter_search(event);" onblur="if (this.value=='')this.value='search';" onfocus="if (this.value=='search')this.value=''"  />
+          </li>
+              <li class="sms-last">
+            <input type="submit" value="" class="submit" />
+            <a href="#"> search</a></li>
+            </ul>
+      </div>
+        </div>
+    <!--Header--> 
+    <!--Content-->
+    <div id="content">
+          <div class="sidebar">
+        <div class="latest-product">
+              <h2>Tata Cara Pemesanan</h2>
+              <ul>
+            <li><img src="assets/images/valky.jpg" style="width: 150px" alt="valky" title="prodcut" />
+                  <div class="p-info"><span>Cara mendaftar</span><small> Lihat</small></div>
+                </li>
+            <li><img src="assets/images/cell.jpg" style="width: 150px" alt="cell" title="product" class="c-cell" />
+                  <div class="p-info"><span>Cara memesan</span><small> Lihat</small></div>
+                </li>
+            <li><img src="assets/images/cell-btn.jpg" style="width: 150px" alt="btn" title="product" class="c-btn" />
+                  <div class="p-info"><span>Cara membayar</span><small> Lihat</small></div>
+                </li>
+          </ul>
+              <a href="#">View More <span> &raquo; </span></a> </div>
+        <div class="latest-product ">
+              <h2>Stok Darah Tersedia</h2>
+              <ul class="info">
+            <li><a href="#">Golongan A</a></li>
+            <li><a href="#">Golongan B</a></li>
+            <li><a href="#">Golongan O</a></li>
+            <li><a href="#">Golongan BO</a></li>
+     
+          </ul>
+                </div>
+        
+      </div>
+          <div class="content-right" >
+        <div class="banner">
+              <div class="slides">
+            <div class="slides_container">
+                  <div class="imgeslider"><img src="assets/images/lapy.jpg" style="width:250px" alt="lapy" title="product" />
+                <div class="banner-text">
+                      <h1>UPT PMI</h1>
+                      <span>Dinas Kesehatan Kabupaten Pandeglang</span>
+                      <p>Untuk mengetahui lebih lanjut ?</p>
+                      <a href="#"> Klik di sini <span></span></a></div>
+              </div>
+                  <div class="imgeslider"><img src="assets/images/lapy.jpg" style="width:250px" alt="lapy" title="product" />
+                <div class="banner-text">
+                      <h1>UPT PMI</h1>
+                      <span>Dinas Kesehatan Kabupaten Pandeglang</span>
+                      <p>Untuk mengetahui lebih lanjut ?</p>
+                      <a href="#"> Klik di sini <span></span></a></div>
+              </div>
+                  <div class="imgeslider"><img src="assets/images/lapy.jpg" style="width:250px" alt="lapy" title="product" />
+                <div class="banner-text">
+                      <h1>UPT PMI</h1>
+                      <span>Dinas Kesehatan Kabupaten Pandeglang</span>
+                      <p>Untuk mengetahui lebih lanjut ?</p>
+                      <a href="#"> Klik di sini <span></span></a></div>
+              </div>
+                  <div class="imgeslider"><img src="assets/images/lapy.jpg" style="width:250px" alt="lapy" title="product" />
+                <div class="banner-text">
+                      <h1>UPT PMI</h1>
+                      <span>Dinas Kesehatan Kabupaten Pandeglang</span>
+                      <p>Untuk mengetahui lebih lanjut ?</p>
+                      <a href="#"> Klik di sini <span></span></a></div>
+              </div>
+                </div>
+          </div>
+            </div>
+        <div class="product-des">
+              <div class="product-info"><img src="assets/images/ipod-tuch.jpg" style="width: 150px; height: 150px" alt="ipod-tuch" title="product" />
+            <h2>Ipod Touch 3G</h2>
+            <div class="add"><span>$2.156.00</span><a href="#">Add To Cart</a></div>
+            <div class="add-to-list blank">
+                  <ul>
+                <li><a href="#">Add To Wish List</a></li>
+                <li><a href="#">+ Add To Compare</a></li>
+              </ul>
+                </div>
+          </div>
+              <div class="product-info"><img src="assets/images/iphone-g.jpg" style="width: 150px; height: 150px" alt="iphone-g" title="product" />
+            <div class="rotatebg">&nbsp;</div>
+            <div class="newrotate">New!</div>
+            <h2>IPhone 4G</h2>
+            <div class="add"><span>$45.166.00</span><a href="#">Add To Cart</a></div>
+            <div class="add-to-list ">
+                  <ul>
+                <li><a href="#">Add To Wish List</a></li>
+                <li><a href="#">+ Add To Compare</a></li>
+              </ul>
+                </div>
+          </div>
+              <div class="product-info last-p"><img src="assets/images/ipod-tuch.jpg" style="width: 150px; height: 150px" alt="ipod-tuch" title="product" />
+            <h2>IPhone 5G</h2>
+            <div class="add"><span>$54.123.00</span><a href="#">Add To Cart</a></div>
+            <div class="add-to-list ">
+                  <ul>
+                <li><a href="#">Add To Wish List</a></li>
+                <li><a href="#">+ Add To Compare</a></li>
+              </ul>
+                </div>
+          </div>
+            </div>
+        <div class="product-des">
+              <div class="product-info"><img src="assets/images/lapy-t.jpg" style="width: 150px; height: 150px" alt="lapy" title="product" class="lapy" />
+            <h2>lAPTOP DV3</h2>
+            <div class="add"><span>$2.156.00</span><a href="#">Add To Cart</a></div>
+            <div class="add-to-list blank">
+                  <ul>
+                <li><a href="#">Add To Wish List</a></li>
+                <li><a href="#">+ Add To Compare</a></li>
+              </ul>
+                </div>
+          </div>
+              <div class="product-info"><img src="assets/images/g-camera.jpg" style="width: 150px; height: 150px" alt="g-camera" title="product" class="lapy" />
+            <h2>Galaxy Camera</h2>
+            <div class="add"><span>$2.156.00</span><a href="#">Add To Cart</a></div>
+            <div class="add-to-list ">
+                  <ul>
+                <li><a href="#">Add To Wish List</a></li>
+                <li><a href="#">+ Add To Compare</a></li>
+              </ul>
+                </div>
+          </div>
+              <div class="product-info last-p"><img src="assets/images/dbl-phone.jpg" style="width: 150px; height: 150px" alt="dbl-ipad" title="product" class="d-phone" />
+            <div class="rotatebg">&nbsp;</div>
+            <div class="newrotate">New!</div>
+            <h2>Htc Evo 4G</h2>
+            <div class="add"><span>$2.156.00</span><a href="#">Add To Cart</a></div>
+            <div class="add-to-list ">
+                  <ul>
+                <li><a href="#">Add To Wish List</a></li>
+                <li><a href="#">+ Add To Compare</a></li>
+              </ul>
+                </div>
+          </div>
+            </div>
+      </div>
+        </div>
+    <!-- Content--> 
+  </div>
+      <!--Footer-->
+      <div id="footer">
+    <div class="footer-top">
+          <div class="page">
+        <div class="f-nav-section">
+              <ul>
+            <li ><a href="index.html">Home </a></li>
+            <li><a href="#">Service </a></li>
+            <li><a href="#">Shops </a></li>
+            <li><a href="#">Reviews</a></li>
+            <li><a href="#">New Products </a></li>
+            <li><a href="#">Specials </a></li>
+            <li><a href="#" class="last">Contact </a></li>
+          </ul>
+              <div class="signup">
+           
+          </div>
+            </div>
+      </div>
+        </div>
+    <div class="page">
+          <div class="footer-bottom">
+        <div class="copyright">
+              <ul>
+            <li>&copy; 2017 </li>
+            <li><a href=" " class="select"> Web</a> Bank Darah.</li>
+            <li class="last"><a href="#">Dewi</a> </li>
+          </ul>
+            </div>
+        <div class="social-icon">
+              <ul>
+            <li><a href="#" class="facebook"></a></li>
+            <li><a href="#" class="twitter"></a></li>
+            <li class="sms-last"><a href="#" class="bing"></a></li>
+          </ul>
+            </div>
+      </div>
+        </div>
+  </div>
+      <!--Footer--> 
+    </div>
+<!-- <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5125d19d7877c409"></script>
+--> 
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script> 
+<!--Wrapper-->
+</body>
+</html>
